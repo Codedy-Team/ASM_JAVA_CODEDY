4bf835b6bb120a95f6c2304cbe261ab028b0051c
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-06-05 14:58:46 +0700

    update edit

--- src/main/java/Controller/dashboard/Edit.java
+++ src/main/java/Controller/dashboard/Edit.java
@@ -1,13 +1,44 @@
 package Controller.dashboard;
 
+import Model.Product;
+import Service.ProductDatabaseService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-@WebServlet(name = "edit", value = "/edit")
+@WebServlet(name = "edit", value = "admin/product/edit")
 public class Edit extends HttpServlet {
+    private ProductDatabaseService productDatabaseService;
+    private DataSource dataSource;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        Context initContext = null;
+        try {
+            initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/asm_codedy");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            productDatabaseService = new ProductDatabaseService(dataSource);
+        } catch (Exception exc) {
+            throw new ServletException(exc);
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -15,6 +46,41 @@ public class Edit extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        int product_category_id = Integer.parseInt(request.getParameter("product_category_id"));
+        int restaurant_id = Integer.parseInt(request.getParameter("restaurant_id"));
+
+        String name = request.getParameter("name");
+        String ingredients = request.getParameter("ingredients");
+        Double price = Double.parseDouble(request.getParameter("price"));
+        String image = request.getParameter("image");
+        String country = request.getParameter("country");
+        String tag = request.getParameter("tag");
+        String description = request.getParameter("description");
+        Boolean featured = Boolean.parseBoolean(request.getParameter("featured"));
+        String created_by = request.getParameter("created_by");
+        java.util.Date created_at = null;
+        try {
+            created_at = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("created_at"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String updated_by = request.getParameter("updated_by");
+        Date updated_at = new Date(Calendar.getInstance().getTime().getTime());
+        int version =  Integer.parseInt(request.getParameter("version"));
+
+        Product product = new Product(id, product_category_id, restaurant_id, name, ingredients,price,image, country, tag,description, featured,
+                created_at,created_by,updated_at, updated_by,version,false );
+
+
+        try {
+            productDatabaseService.update(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect(request.getContextPath() + "/product-menu/show/?id=" + id);
 
     }
 }
--- src/main/java/Service/ProductDatabaseService.java
+++ src/main/java/Service/ProductDatabaseService.java
@@ -1,10 +1,19 @@
 package Service;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import Model.Product;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ProductDatabaseService {
+
+    private DataSource dataSource;
+
+    public ProductDatabaseService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     private void close(Connection connection, Statement statement, ResultSet resultSet) {
 
@@ -24,6 +33,48 @@ public class ProductDatabaseService {
             }
         } catch (Exception exc) {
             exc.printStackTrace();
+        }
+    }
+
+    public void update(Product item) throws Exception {
+
+        Connection myConn = null;
+        PreparedStatement myStmt = null;
+
+        try {
+            // get db connection
+            myConn = dataSource.getConnection();
+
+            // create SQL update statement
+            String sql = "UPDATE product "
+                    + "SET product_category_id = ?, restaurant_id = ?, name = ?, ingredients = ?, price = ?, image = ?, country = ?, tag = ?, description = ?, featured = ?, updated_at = ?, updated_by = ?, version = ?, deleted =?"
+                    + "WHERE id = ?";
+
+            // prepare statement
+            myStmt = myConn.prepareStatement(sql);
+
+            // set params
+            myStmt.setInt(1, item.getProductCategoryId());
+            myStmt.setInt(2, item.getRestaurantId());
+            myStmt.setString(3, item.getName());
+            myStmt.setString(4, item.getIngredients());
+            myStmt.setDouble(5, item.getPrice());
+            myStmt.setString(6, item.getImage());
+            myStmt.setString(7, item.getCountry());
+            myStmt.setString(8, item.getTag());
+            myStmt.setString(9, item.getDescription());
+            myStmt.setBoolean(10, item.getFeatured());
+            myStmt.setDate(11, new Date(Calendar.getInstance().getTime().getTime()));
+            myStmt.setString(12, "CODEDY");
+            myStmt.setInt(13, item.getVersion() + 1);
+            myStmt.setBoolean(14, item.getDeleted());
+
+            // execute SQL statement
+            myStmt.execute();
+        }
+        finally {
+            // clean up JDBC objects
+            close(myConn, myStmt, null);
         }
     }
 }
